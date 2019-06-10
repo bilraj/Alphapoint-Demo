28f5432a83aed323cd495c553540bbd9e39edd0c
Bilal Raja <bilraj@umich.edu>
2019-06-10 13:05:08 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route } from 'react-router-dom';
+
+import ProductList from './store/ProductList';
+import Details from './store/Details';
+// import Cart from './store/Cart';
+import Navbar from './store/Navbar';
+import Cart from './store/Cart';
+import Def from './store/Def';
+import Modal from './store/Modal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route exact path="/details" component={Details} />
+        <Route exact path="/cart" component={Cart} />
+        <Route component={Def} />
+      </Switch>
+      <Modal />
+
     </div>
+
   );
 }
 
